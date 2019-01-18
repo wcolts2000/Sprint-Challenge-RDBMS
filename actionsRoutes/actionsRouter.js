8e3b0c7f3fcec33bd040885c795bb99892ef6866
wcolts2000 <wcolts2000@gmail.com>
2019-01-18 11:02:45 -0800

    added crud functionality for actoins

--- actionsRoutes/actionsRouter.js
+++ actionsRoutes/actionsRouter.js
@@ -34,4 +34,40 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({ message: "error getting data"}))
 })
 
+// GET SINGLE ACTION
+router.get('/:id', (req, res) => {
+  db('actions')
+  .where({ id: req.params.id})
+  .then(action => {
+    if(action.length) {
+      res.status(200).json(action)
+    } else { res.status(404).json({ message: "no action by that id found"})}
+  })
+  .catch(err => res.status(500).json({ messgae: " three was an error retrieving the data"}))
+})
+
+// Update Action
+router.put('/:id', (req,res) => {
+  db('actions')
+  .where({ id: req.params.id})
+  .then(count => {
+    if(count) {
+      res.status(200).json(count)
+    } else {res.status(404).json({ message: "no action found by that id"})}
+  })
+  .catch( err => res.status(500).json({ message: "there was an error updating the action"}))
+})
+
+// Delete Action
+router.delete('/:id', (req, res) => {
+  db('actions')
+  .where({ id: req.params.id})
+  .then(count => {
+    if(count) {
+      res.status(200).json(count)
+    } else {res.status(404).json({ message: "no action found by that id"})}
+  })
+  .catch(err => res.status(500).json({ message: "there was an error deleting"}))
+})
+
 module.exports = router;
