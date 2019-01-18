54177805f838a2f79dc4cdcb8e9ae75d5724453a
wcolts2000 <wcolts2000@gmail.com>
2019-01-18 08:46:32 -0800

    migrations working on latest and rollback without errors

--- data/migrations/20190118083434_001-projects.js
+++ data/migrations/20190118083434_001-projects.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('projects', function(tbl) {
+    tbl.increments();
+
+    tbl.string('name', 255).notNullable()
+    tbl.text('description', 255).notNullable()
+    tbl.boolean('complete')
+    tbl.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('projects');
 };
